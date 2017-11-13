8559d23b8ca3664a06d286737d46ba9cf5f978f0
Hugo1030 <yzhjsw520@gmail.com>
2017-11-14 07:33:01 +0800

    course to xadmin

--- MxOnline/apps/courses/models.py
+++ MxOnline/apps/courses/models.py
@@ -8,7 +8,7 @@ class Course(models.Model):
     name = models.CharField(max_length=50, verbose_name="课程名")
     desc = models.CharField(max_length=300, verbose_name="课程描述")
     detail = models.TextField(verbose_name="课程详情")
-    degree = models.CharField(choices=(("cj", "初级"),("zj", "中级"),("gj", "高级")), max_length=2)
+    degree = models.CharField(verbose_name="难度", choices=(("cj", "初级"),("zj", "中级"),("gj", "高级")), max_length=2)
     learn_times = models.IntegerField(default=0, verbose_name="学习时长(分钟数)")
     students = models.IntegerField(default=0, verbose_name="学习人数")
     fav_nums = models.IntegerField(default=0, verbose_name="收藏人数")
@@ -20,6 +20,9 @@ class Course(models.Model):
         verbose_name = "课程"
         verbose_name_plural = verbose_name
 
+    def __str__(self):
+        return self.name
+
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, verbose_name="课程")
@@ -27,7 +30,7 @@ class Lesson(models.Model):
     add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
 
     class Meta:
-        verbose_name = "课程"
+        verbose_name = "章节"
         verbose_name_plural = verbose_name
 
 class Video(models.Model):
