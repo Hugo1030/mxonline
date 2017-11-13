c81977eea646d3e97981a725bbace3d3ac61f6f5
Hugo1030 <yzhjsw520@gmail.com>
2017-11-13 15:13:23 +0800

    debug

--- MxOnline/MxOnline/settings.py
+++ MxOnline/MxOnline/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
     'courses'
 ]
 AUTH_USER_MODEL = 'users.UserProfile'
